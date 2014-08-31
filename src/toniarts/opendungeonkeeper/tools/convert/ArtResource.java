522335a39081f788226ae776e5bf234a0bc0f158
Petteri Loisko <wizandoz@gmail.com>
2014-08-31 20:00:31 +0300

    Stub for the ArtResource type.

---
+++ src/toniarts/opendungeonkeeper/tools/convert/ArtResource.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert;
+
+/**
+ *
+ * @author Wizand
+ */
+public class ArtResource {
+    
+}
