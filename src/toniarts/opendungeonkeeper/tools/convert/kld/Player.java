a22941ad3e7fa34919d749cb2cc3d7633f7dc229
Petteri Loisko <wizandoz@gmail.com>
2014-09-02 18:22:24 +0300

    Accessors/mutators

--- src/toniarts/opendungeonkeeper/tools/convert/kld/Player.java
+++ src/toniarts/opendungeonkeeper/tools/convert/kld/Player.java
@@ -4,6 +4,7 @@
  */
 package toniarts.opendungeonkeeper.tools.convert.kld;
 
+
 /**
  * Barely started placeholder for the container class for the mapnamePlayer.kld
  * 
@@ -15,8 +16,22 @@ package toniarts.opendungeonkeeper.tools.convert.kld;
  */
 public class Player {
     
-    /**
-     * struct PlayerBlock {
+    private PlayerBlock playerBlock;
+
+    public PlayerBlock getPlayerBlock() {
+        return playerBlock;
+    }
+
+    public void setPlayerBlock(PlayerBlock playerBlock) {
+        this.playerBlock = playerBlock;
+    }
+    
+    
+    
+}
+class PlayerBlock {
+  /**
+  struct PlayerBlock {
   int x00;
   int x04;
   uint8_t x08[158];
@@ -35,5 +50,71 @@ public class Player {
     private short xa9;
     private short xab;
     private  String name;
+
+    public int getX00() {
+        return x00;
+    }
+
+    public void setX00(int x00) {
+        this.x00 = x00;
+    }
+
+    public int getX04() {
+        return x04;
+    }
+
+    public void setX04(int x04) {
+        this.x04 = x04;
+    }
+
+    public byte[] getX08() {
+        return x08;
+    }
+
+    public void setX08(byte[] x08) {
+        this.x08 = x08;
+    }
+
+    public short getXa6() {
+        return xa6;
+    }
+
+    public void setXa6(short xa6) {
+        this.xa6 = xa6;
+    }
+
+    public byte getIdPlayer() {
+        return idPlayer;
+    }
+
+    public void setIdPlayer(byte idPlayer) {
+        this.idPlayer = idPlayer;
+    }
+
+    public short getXa9() {
+        return xa9;
+    }
+
+    public void setXa9(short xa9) {
+        this.xa9 = xa9;
+    }
+
+    public short getXab() {
+        return xab;
+    }
+
+    public void setXab(short xab) {
+        this.xab = xab;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
     
 }
--- src/toniarts/opendungeonkeeper/tools/convert/kld/Terrain.java
+++ src/toniarts/opendungeonkeeper/tools/convert/kld/Terrain.java
@@ -27,7 +27,8 @@ public class Terrain {
     }
 
 }
-class TerrainBlock {/**
+class TerrainBlock {
+    /**
      * struct Terrain {
   char name[32]; /* 0 */
 //  ArtResource complete; /* 20 */
@@ -108,6 +109,272 @@ class TerrainBlock {/**
   private byte[] ambient_light; /* 201 */
   private String str2; /* 204 */
   private int unk224; /* 224 */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUnk188() {
+        return unk188;
+    }
+
+    public void setUnk188(int unk188) {
+        this.unk188 = unk188;
+    }
+
+    public int getLight_height() {
+        return light_height;
+    }
+
+    public void setLight_height(int light_height) {
+        this.light_height = light_height;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+
+    public void setFlags(int flags) {
+        this.flags = flags;
+    }
+
+    public short getDamage() {
+        return damage;
+    }
+
+    public void setDamage(short damage) {
+        this.damage = damage;
+    }
+
+    public short getUnk196() {
+        return unk196;
+    }
+
+    public void setUnk196(short unk196) {
+        this.unk196 = unk196;
+    }
+
+    public short getUnk198() {
+        return unk198;
+    }
+
+    public void setUnk198(short unk198) {
+        this.unk198 = unk198;
+    }
+
+    public short getGold_value() {
+        return gold_value;
+    }
+
+    public void setGold_value(short gold_value) {
+        this.gold_value = gold_value;
+    }
+
+    public short getMana_gain() {
+        return mana_gain;
+    }
+
+    public void setMana_gain(short mana_gain) {
+        this.mana_gain = mana_gain;
+    }
+
+    public short getMax_mana_gain() {
+        return max_mana_gain;
+    }
+
+    public void setMax_mana_gain(short max_mana_gain) {
+        this.max_mana_gain = max_mana_gain;
+    }
+
+    public short getUnk1a0() {
+        return unk1a0;
+    }
+
+    public void setUnk1a0(short unk1a0) {
+        this.unk1a0 = unk1a0;
+    }
+
+    public short getUnk1a2() {
+        return unk1a2;
+    }
+
+    public void setUnk1a2(short unk1a2) {
+        this.unk1a2 = unk1a2;
+    }
+
+    public short getUnk1a4() {
+        return unk1a4;
+    }
+
+    public void setUnk1a4(short unk1a4) {
+        this.unk1a4 = unk1a4;
+    }
+
+    public short getUnk1a6() {
+        return unk1a6;
+    }
+
+    public void setUnk1a6(short unk1a6) {
+        this.unk1a6 = unk1a6;
+    }
+
+    public short getUnk1a8() {
+        return unk1a8;
+    }
+
+    public void setUnk1a8(short unk1a8) {
+        this.unk1a8 = unk1a8;
+    }
+
+    public short getUnk1aa() {
+        return unk1aa;
+    }
+
+    public void setUnk1aa(short unk1aa) {
+        this.unk1aa = unk1aa;
+    }
+
+    public short getUnk1ac() {
+        return unk1ac;
+    }
+
+    public void setUnk1ac(short unk1ac) {
+        this.unk1ac = unk1ac;
+    }
+
+    public short[] getUnk1ae() {
+        return unk1ae;
+    }
+
+    public void setUnk1ae(short[] unk1ae) {
+        this.unk1ae = unk1ae;
+    }
+
+    public byte getWibble_h() {
+        return wibble_h;
+    }
+
+    public void setWibble_h(byte wibble_h) {
+        this.wibble_h = wibble_h;
+    }
+
+    public byte[] getLean_h() {
+        return lean_h;
+    }
+
+    public void setLean_h(byte[] lean_h) {
+        this.lean_h = lean_h;
+    }
+
+    public byte getWibble_v() {
+        return wibble_v;
+    }
+
+    public void setWibble_v(byte wibble_v) {
+        this.wibble_v = wibble_v;
+    }
+
+    public byte[] getLean_v() {
+        return lean_v;
+    }
+
+    public void setLean_v(byte[] lean_v) {
+        this.lean_v = lean_v;
+    }
+
+    public byte getId() {
+        return id;
+    }
+
+    public void setId(byte id) {
+        this.id = id;
+    }
+
+    public short getStarting_health() {
+        return starting_health;
+    }
+
+    public void setStarting_health(short starting_health) {
+        this.starting_health = starting_health;
+    }
+
+    public byte getMax_health_type() {
+        return max_health_type;
+    }
+
+    public void setMax_health_type(byte max_health_type) {
+        this.max_health_type = max_health_type;
+    }
+
+    public byte getDestroyed_type() {
+        return destroyed_type;
+    }
+
+    public void setDestroyed_type(byte destroyed_type) {
+        this.destroyed_type = destroyed_type;
+    }
+
+    public byte[] getTerrain_light() {
+        return terrain_light;
+    }
+
+    public void setTerrain_light(byte[] terrain_light) {
+        this.terrain_light = terrain_light;
+    }
+
+    public byte getTexture_frames() {
+        return texture_frames;
+    }
+
+    public void setTexture_frames(byte texture_frames) {
+        this.texture_frames = texture_frames;
+    }
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public short getMax_health() {
+        return max_health;
+    }
+
+    public void setMax_health(short max_health) {
+        this.max_health = max_health;
+    }
+
+    public byte[] getAmbient_light() {
+        return ambient_light;
+    }
+
+    public void setAmbient_light(byte[] ambient_light) {
+        this.ambient_light = ambient_light;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
+
+    public int getUnk224() {
+        return unk224;
+    }
+
+    public void setUnk224(int unk224) {
+        this.unk224 = unk224;
+    }
     
     
+  
+  
 }
