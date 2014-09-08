0a6f8786950f8078adadb68ce221cc158916a303
Toni <Toni@192.168.0.100>
2014-09-08 23:09:38 +0300

    Supports loading groups

--- src/toniarts/opendungeonkeeper/tools/convert/KmfModelLoader.java
+++ src/toniarts/opendungeonkeeper/tools/convert/KmfModelLoader.java
@@ -7,11 +7,13 @@ package toniarts.opendungeonkeeper.tools.convert;
 import com.jme3.asset.AssetInfo;
 import com.jme3.asset.AssetKey;
 import com.jme3.asset.AssetLoader;
+import com.jme3.asset.ModelKey;
 import com.jme3.export.binary.BinaryExporter;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.AssetLinkNode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
@@ -33,6 +35,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import toniarts.opendungeonkeeper.tools.convert.kmf.Grop;
 import toniarts.opendungeonkeeper.tools.convert.kmf.KmfFile;
 import toniarts.opendungeonkeeper.tools.convert.kmf.MeshSprite;
 import toniarts.opendungeonkeeper.tools.convert.kmf.MeshVertex;
@@ -119,122 +122,146 @@ public class KmfModelLoader implements AssetLoader {
             kmfFile = new KmfFile(inputStreamToFile(assetInfo.openStream(), assetInfo.getKey().getName()));
         }
 
-        // Create the materials
-        HashMap<Integer, Material> materials = new HashMap(kmfFile.getMaterials().size());
-        int i = 0;
-        for (toniarts.opendungeonkeeper.tools.convert.kmf.Material mat : kmfFile.getMaterials()) {
-            Material material = new Material(assetInfo.getManager(), "Common/MatDefs/Light/Lighting.j3md");
-            String texture = mat.getTextures().get(0);
-            if (textureFixes.containsKey(texture)) {
-
-                //Fix the texture entry
-                texture = textureFixes.get(texture);
-            }
-            Texture tex = assetInfo.getManager().loadTexture(AssetsConverter.TEXTURES_FOLDER.concat("/").concat(texture).concat(".png"));
-            material.setTexture("DiffuseMap", tex);
-            material.setColor("Specular", ColorRGBA.Orange); // Dungeons are lit only with fire...? Experimental
-            material.setColor("Diffuse", ColorRGBA.White); // Experimental
-            material.setFloat("Shininess", 128 * mat.getBrightness()); // Use the brightness as shininess... Experimental
-            materials.put(i, material);
-            i++;
-        }
-
         //Create a root
         Node root = new Node("Root");
 
-        //
-        // The meshes
-        //
-        for (toniarts.opendungeonkeeper.tools.convert.kmf.Mesh sourceMesh : kmfFile.getMeshes()) {
+        if (kmfFile.getType() == KmfFile.Type.MESH) {
 
-            //Source mesh is node
-            Node node = new Node(sourceMesh.getName());
-            node.setLocalScale(sourceMesh.getScale());
-            node.setLocalTranslation(new Vector3f(sourceMesh.getPos().x, sourceMesh.getPos().y, sourceMesh.getPos().z));
+            //
+            // Create the materials
+            //
+            HashMap<Integer, Material> materials = new HashMap(kmfFile.getMaterials().size());
+            int i = 0;
+            for (toniarts.opendungeonkeeper.tools.convert.kmf.Material mat : kmfFile.getMaterials()) {
+                Material material = new Material(assetInfo.getManager(), "Common/MatDefs/Light/Lighting.j3md");
+                String texture = mat.getTextures().get(0);
+                if (textureFixes.containsKey(texture)) {
 
-            int index = 0;
-            for (MeshSprite meshSprite : sourceMesh.getSprites()) {
-
-                //Each sprite represents a geometry (+ mesh) since they each have their own material
-                Mesh mesh = new Mesh();
-
-                //Vertices, UV (texture coordinates), normals
-                Vector3f[] vertices = new Vector3f[meshSprite.getVertices().size()];
-                Vector2f[] texCoord = new Vector2f[meshSprite.getVertices().size()];
-                Vector3f[] normals = new Vector3f[meshSprite.getVertices().size()];
-                i = 0;
-                for (MeshVertex meshVertex : meshSprite.getVertices()) {
-
-                    //Vertice
-                    javax.vecmath.Vector3f v = sourceMesh.getGeometries().get(meshVertex.getGeomIndex());
-                    vertices[i] = new Vector3f(v.x, v.y, v.z);
-
-                    //Texture coordinate
-                    Uv uv = meshVertex.getUv();
-                    texCoord[i] = new Vector2f(uv.getUv()[0] / 32768f, uv.getUv()[1] / 32768f);
-
-                    //Normals
-                    v = meshVertex.getNormal();
-                    normals[i] = new Vector3f(v.x, v.y, v.z);
-
-                    i++;
+                    //Fix the texture entry
+                    texture = textureFixes.get(texture);
                 }
-
-                // Triangles, we have LODs here
-                VertexBuffer[] lodLevels = new VertexBuffer[meshSprite.getTriangles().size()];
-                for (Entry<Integer, List<Triangle>> triangles : meshSprite.getTriangles().entrySet()) {
-                    int[] indexes = new int[triangles.getValue().size() * 3];
-                    int x = 0;
-                    for (Triangle triangle : triangles.getValue()) {
-                        indexes[x * 3] = triangle.getTriangle()[2];
-                        indexes[x * 3 + 1] = triangle.getTriangle()[1];
-                        indexes[x * 3 + 2] = triangle.getTriangle()[0];
-                        x++;
-                    }
-                    VertexBuffer buf = new VertexBuffer(Type.Index);
-                    buf.setupData(VertexBuffer.Usage.Static, 3, VertexBuffer.Format.UnsignedInt, BufferUtils.createIntBuffer(indexes));
-                    lodLevels[triangles.getKey()] = buf;
-                }
-
-                //Max LOD level triangles
-                List<Integer> faces = new ArrayList<>(meshSprite.getTriangles().get(0).size() * 3);
-                for (Triangle tri : meshSprite.getTriangles().get(0)) {
-                    faces.add(new Short(tri.getTriangle()[2]).intValue());
-                    faces.add(new Short(tri.getTriangle()[1]).intValue());
-                    faces.add(new Short(tri.getTriangle()[0]).intValue());
-                }
-                int[] indexes = new int[faces.size()];
-                int x = 0;
-                for (Integer inte : faces) {
-                    indexes[x] = inte;
-                    x++;
-                }
-
-                //Set the buffers
-                mesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
-                mesh.setBuffer(Type.Index, 3, BufferUtils.createIntBuffer(indexes));
-                mesh.setLodLevels(lodLevels);
-                mesh.setBuffer(Type.TexCoord, 2, BufferUtils.createFloatBuffer(texCoord));
-                mesh.setBuffer(Type.Normal, 3, BufferUtils.createFloatBuffer(normals));
-
-                mesh.updateBound();
-
-                //Create geometry
-                Geometry geom = new Geometry(index + "", mesh);
-
-                // Material
-                geom.setMaterial(materials.get(meshSprite.getMaterialIndex()));
-
-                //Attach the geometry to the node
-                node.attachChild(geom);
-                index++;
+                Texture tex = assetInfo.getManager().loadTexture(AssetsConverter.TEXTURES_FOLDER.concat("/").concat(texture).concat(".png"));
+                material.setTexture("DiffuseMap", tex);
+                material.setColor("Specular", ColorRGBA.Orange); // Dungeons are lit only with fire...? Experimental
+                material.setColor("Diffuse", ColorRGBA.White); // Experimental
+                material.setFloat("Shininess", 128 * mat.getBrightness()); // Use the brightness as shininess... Experimental
+                materials.put(i, material);
+                i++;
             }
 
-            //Attach the node to the root
-            root.attachChild(node);
+            //
+            // The meshes
+            //
+            for (toniarts.opendungeonkeeper.tools.convert.kmf.Mesh sourceMesh : kmfFile.getMeshes()) {
+
+                //Source mesh is node
+                Node node = new Node(sourceMesh.getName());
+                node.setLocalScale(sourceMesh.getScale());
+                node.setLocalTranslation(new Vector3f(sourceMesh.getPos().x, sourceMesh.getPos().y, sourceMesh.getPos().z));
+
+                int index = 0;
+                for (MeshSprite meshSprite : sourceMesh.getSprites()) {
+
+                    //Each sprite represents a geometry (+ mesh) since they each have their own material
+                    Mesh mesh = new Mesh();
+
+                    //Vertices, UV (texture coordinates), normals
+                    Vector3f[] vertices = new Vector3f[meshSprite.getVertices().size()];
+                    Vector2f[] texCoord = new Vector2f[meshSprite.getVertices().size()];
+                    Vector3f[] normals = new Vector3f[meshSprite.getVertices().size()];
+                    i = 0;
+                    for (MeshVertex meshVertex : meshSprite.getVertices()) {
+
+                        //Vertice
+                        javax.vecmath.Vector3f v = sourceMesh.getGeometries().get(meshVertex.getGeomIndex());
+                        vertices[i] = new Vector3f(v.x, v.y, v.z);
+
+                        //Texture coordinate
+                        Uv uv = meshVertex.getUv();
+                        texCoord[i] = new Vector2f(uv.getUv()[0] / 32768f, uv.getUv()[1] / 32768f);
+
+                        //Normals
+                        v = meshVertex.getNormal();
+                        normals[i] = new Vector3f(v.x, v.y, v.z);
+
+                        i++;
+                    }
+
+                    // Triangles, we have LODs here
+                    VertexBuffer[] lodLevels = new VertexBuffer[meshSprite.getTriangles().size()];
+                    for (Entry<Integer, List<Triangle>> triangles : meshSprite.getTriangles().entrySet()) {
+                        int[] indexes = new int[triangles.getValue().size() * 3];
+                        int x = 0;
+                        for (Triangle triangle : triangles.getValue()) {
+                            indexes[x * 3] = triangle.getTriangle()[2];
+                            indexes[x * 3 + 1] = triangle.getTriangle()[1];
+                            indexes[x * 3 + 2] = triangle.getTriangle()[0];
+                            x++;
+                        }
+                        VertexBuffer buf = new VertexBuffer(Type.Index);
+                        buf.setupData(VertexBuffer.Usage.Static, 3, VertexBuffer.Format.UnsignedInt, BufferUtils.createIntBuffer(indexes));
+                        lodLevels[triangles.getKey()] = buf;
+                    }
+
+                    //Max LOD level triangles
+                    List<Integer> faces = new ArrayList<>(meshSprite.getTriangles().get(0).size() * 3);
+                    for (Triangle tri : meshSprite.getTriangles().get(0)) {
+                        faces.add(new Short(tri.getTriangle()[2]).intValue());
+                        faces.add(new Short(tri.getTriangle()[1]).intValue());
+                        faces.add(new Short(tri.getTriangle()[0]).intValue());
+                    }
+                    int[] indexes = new int[faces.size()];
+                    int x = 0;
+                    for (Integer inte : faces) {
+                        indexes[x] = inte;
+                        x++;
+                    }
+
+                    //Set the buffers
+                    mesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
+                    mesh.setBuffer(Type.Index, 3, BufferUtils.createIntBuffer(indexes));
+                    mesh.setLodLevels(lodLevels);
+                    mesh.setBuffer(Type.TexCoord, 2, BufferUtils.createFloatBuffer(texCoord));
+                    mesh.setBuffer(Type.Normal, 3, BufferUtils.createFloatBuffer(normals));
+
+                    mesh.updateBound();
+
+                    //Create geometry
+                    Geometry geom = new Geometry(index + "", mesh);
+
+                    // Material
+                    geom.setMaterial(materials.get(meshSprite.getMaterialIndex()));
+
+                    //Attach the geometry to the node
+                    node.attachChild(geom);
+                    index++;
+                }
+
+                //Attach the node to the root
+                root.attachChild(node);
+            }
+        } else if (kmfFile.getType() == KmfFile.Type.GROP) {
+            createGroup(root, kmfFile);
         }
 
         return root;
+    }
+
+    /**
+     * Creates a grop, a.k.a. a group, links existing models to one scene?
+     *
+     * @param root root node
+     * @param kmfFile the KMF file
+     */
+    private void createGroup(Node root, KmfFile kmfFile) {
+
+        //Go trough the models and add them
+        for (Grop grop : kmfFile.getGrops()) {
+            String key = AssetsConverter.MODELS_FOLDER.concat("/").concat(grop.getName()).concat(".j3o");
+            AssetLinkNode modelLink = new AssetLinkNode(key, new ModelKey(key));
+            modelLink.setLocalTranslation(new Vector3f(grop.getPos().x, grop.getPos().y, grop.getPos().z));
+            root.attachChild(modelLink);
+        }
     }
 
     /**
