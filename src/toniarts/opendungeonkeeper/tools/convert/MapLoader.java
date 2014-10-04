cf2f0f8b8653fbf91e58403cc7ee2092b9a6a7a6
Toni Helenius <helenius.toni@gmail.com>
2014-10-04 12:32:21 +0300

    Loader for map files, easy to test the reading with this tool

---
+++ src/toniarts/opendungeonkeeper/tools/convert/MapLoader.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert;
+
+import java.io.File;
+import toniarts.opendungeonkeeper.tools.convert.kld.KwdFile;
+
+/**
+ * A simple class to load up a map file
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class MapLoader {
+
+    public static void main(String[] args) {
+
+        //Take Dungeon Keeper 2 root folder as parameter
+        if (args.length != 2 && !new File(args[0]).exists()) {
+            throw new RuntimeException("Please provide Dungeon Keeper II root folder as a first parameter! Second parameter is the actual map file!");
+        }
+
+        //Form the data path
+        String dataDirectory = args[0];
+        if (!dataDirectory.endsWith(File.separator)) {
+            dataDirectory = dataDirectory.concat(File.separator);
+        }
+
+        //Load the map
+        KwdFile kwd = new KwdFile(dataDirectory);
+        kwd.loadMap(new File(args[1]));
+    }
+}
