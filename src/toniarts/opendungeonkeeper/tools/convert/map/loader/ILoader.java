0899ce2d54d0ee6bc2fb1b2647cd160b819bedbd
Toni Helenius <helenius.toni@gmail.com>
2014-10-14 22:19:33 +0300

    Experimental loaders to load up DK II map classes

---
+++ src/toniarts/opendungeonkeeper/tools/convert/map/loader/ILoader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert.map.loader;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Spatial;
+
+/**
+ * A simple interface for loading DK2 map classes to JME spatials
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public interface ILoader<T> {
+
+    public Spatial load(AssetManager assetManager, T object);
+}
---
+++ src/toniarts/opendungeonkeeper/tools/convert/map/loader/MapLoader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert.map.loader;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Spatial;
+import toniarts.opendungeonkeeper.tools.convert.map.KwdFile;
+
+/**
+ * Loads whole maps
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class MapLoader implements ILoader<KwdFile> {
+
+    @Override
+    public Spatial load(AssetManager assetManager, KwdFile object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
