77ec36cd0d45c4014f0acafe9915ef6e21a2c46d
Toni Helenius <helenius.toni@gmail.com>
2014-11-13 13:14:31 +0200

    Interface for the flags

---
+++ src/toniarts/opendungeonkeeper/tools/convert/map/IFlagEnum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert.map;
+
+/**
+ * Interface for enums dealing with map flags
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public interface IFlagEnum {
+
+    public int getFlagValue();
+}
