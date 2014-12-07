4ac210a2d0be744d7fff91cec7e4ec7b7f605519
Toni Helenius <helenius.toni@gmail.com>
2014-12-07 12:01:49 +0200

    Also takes in the texture entry file

--- src/toniarts/opendungeonkeeper/tools/convert/KmfAssetInfo.java
+++ src/toniarts/opendungeonkeeper/tools/convert/KmfAssetInfo.java
@@ -8,21 +8,24 @@ import com.jme3.asset.AssetInfo;
 import com.jme3.asset.AssetKey;
 import com.jme3.asset.AssetManager;
 import java.io.InputStream;
+import toniarts.opendungeonkeeper.tools.convert.enginetextures.EngineTexturesFile;
 import toniarts.opendungeonkeeper.tools.convert.kmf.KmfFile;
 
 /**
  * Small wrapper class to avoid writing and reading KMF files all over again
- * during conversion
+ * during conversion (and textures)
  *
  * @author Toni Helenius <helenius.toni@gmail.com>
  */
 public class KmfAssetInfo extends AssetInfo {
 
     private final KmfFile kmfFile;
+    private final EngineTexturesFile engineTexturesFile;
 
-    public KmfAssetInfo(AssetManager manager, AssetKey key, KmfFile kmfFile) {
+    public KmfAssetInfo(AssetManager manager, AssetKey key, KmfFile kmfFile, EngineTexturesFile engineTexturesFile) {
         super(manager, key);
         this.kmfFile = kmfFile;
+        this.engineTexturesFile = engineTexturesFile;
     }
 
     @Override
@@ -32,5 +35,9 @@ public class KmfAssetInfo extends AssetInfo {
 
     public KmfFile getKmfFile() {
         return kmfFile;
+    }
+
+    public EngineTexturesFile getEngineTexturesFile() {
+        return engineTexturesFile;
     }
 }
