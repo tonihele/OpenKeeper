af24a0f542e7c496dba0a40135b7736d36d2c6b1
Toni Helenius <helenius.toni@gmail.com>
2014-12-08 22:24:49 +0200

    Added toString, equals & hashCode

--- src/toniarts/opendungeonkeeper/tools/convert/kmf/Material.java
+++ src/toniarts/opendungeonkeeper/tools/convert/kmf/Material.java
@@ -5,6 +5,7 @@
 package toniarts.opendungeonkeeper.tools.convert.kmf;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * KMF Material wrapper
@@ -66,5 +67,52 @@ public class Material {
 
     protected void setEnvironmentMappingTexture(String environmentMappingTexture) {
         this.environmentMappingTexture = environmentMappingTexture;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + Objects.hashCode(this.textures);
+        hash = 41 * hash + this.flag;
+        hash = 41 * hash + Float.floatToIntBits(this.brightness);
+        hash = 41 * hash + Float.floatToIntBits(this.gamma);
+        hash = 41 * hash + Objects.hashCode(this.environmentMappingTexture);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Material other = (Material) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.textures, other.textures)) {
+            return false;
+        }
+        if (this.flag != other.flag) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.brightness) != Float.floatToIntBits(other.brightness)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.gamma) != Float.floatToIntBits(other.gamma)) {
+            return false;
+        }
+        if (!Objects.equals(this.environmentMappingTexture, other.environmentMappingTexture)) {
+            return false;
+        }
+        return true;
     }
 }
