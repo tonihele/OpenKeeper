23ffb981dbbbefac438ffee15a9756c25c527d4d
Toni Helenius <helenius.toni@gmail.com>
2014-12-26 16:27:30 +0200

    Equals & hashCode now case insensitive and only consisting name + texture There are values that differ, but I think it is not worth supporting those varities, the artists can easily meddle with the materials this way

--- src/toniarts/opendungeonkeeper/tools/convert/kmf/Material.java
+++ src/toniarts/opendungeonkeeper/tools/convert/kmf/Material.java
@@ -4,6 +4,7 @@
  */
 package toniarts.opendungeonkeeper.tools.convert.kmf;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -69,6 +70,32 @@ public class Material {
         this.environmentMappingTexture = environmentMappingTexture;
     }
 
+    public boolean equalsIgnoreCase(List<String> l1, List<String> l2) {
+
+        // Null check
+        if (l1 == null && l2 == null) {
+            return true;
+        }
+        if ((l1 == null && l2 != null) || (l2 == null && l1 != null)) {
+            return false;
+        }
+
+        // Size check
+        if (l1.size() != l2.size()) {
+            return false;
+        }
+
+        // Go through the individual items
+        Iterator<String> i1 = l1.iterator();
+        Iterator<String> i2 = l2.iterator();
+        while (i1.hasNext()) {
+            if (!i1.next().equalsIgnoreCase(i2.next())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         return name;
@@ -77,12 +104,8 @@ public class Material {
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 41 * hash + Objects.hashCode(this.name);
-        hash = 41 * hash + Objects.hashCode(this.textures);
-        hash = 41 * hash + this.flag;
-        hash = 41 * hash + Float.floatToIntBits(this.brightness);
-        hash = 41 * hash + Float.floatToIntBits(this.gamma);
-        hash = 41 * hash + Objects.hashCode(this.environmentMappingTexture);
+        hash = 41 * hash + Objects.hashCode(this.name.toLowerCase());
+        hash = 41 * hash + Objects.hashCode((this.textures != null && this.textures.size() > 0 ? this.textures.get(0).toLowerCase() : this.textures));
         return hash;
     }
 
@@ -95,22 +118,10 @@ public class Material {
             return false;
         }
         final Material other = (Material) obj;
-        if (!Objects.equals(this.name, other.name)) {
+        if (!Objects.equals((this.name != null ? this.name.toLowerCase() : null), (other.name != null ? other.name.toLowerCase() : null))) {
             return false;
         }
-        if (!Objects.equals(this.textures, other.textures)) {
-            return false;
-        }
-        if (this.flag != other.flag) {
-            return false;
-        }
-        if (Float.floatToIntBits(this.brightness) != Float.floatToIntBits(other.brightness)) {
-            return false;
-        }
-        if (Float.floatToIntBits(this.gamma) != Float.floatToIntBits(other.gamma)) {
-            return false;
-        }
-        if (!Objects.equals(this.environmentMappingTexture, other.environmentMappingTexture)) {
+        if (!equalsIgnoreCase(this.textures, other.textures)) {
             return false;
         }
         return true;
