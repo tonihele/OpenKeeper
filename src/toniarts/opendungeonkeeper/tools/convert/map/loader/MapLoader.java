41d92380d1e5b778de07c5004ae930820fd8a528
Toni Helenius <helenius.toni@gmail.com>
2015-01-14 21:13:07 +0200

    Tweaks and fixes, still very crude and handles a fraction of the cases

--- src/toniarts/opendungeonkeeper/tools/convert/map/loader/MapLoader.java
+++ src/toniarts/opendungeonkeeper/tools/convert/map/loader/MapLoader.java
@@ -8,6 +8,7 @@ import com.jme3.asset.AssetManager;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.BatchNode;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import toniarts.opendungeonkeeper.tools.convert.AssetsConverter;
@@ -32,7 +33,7 @@ public class MapLoader implements ILoader<KwdFile> {
         this.kwdFile = object;
 
         //Create a root
-        Node root = new Node("Map");
+        BatchNode root = new BatchNode("Map");
 
         // Go through the map
         Map[][] tiles = object.getTiles();
@@ -59,63 +60,89 @@ public class MapLoader implements ILoader<KwdFile> {
                     }
 
                     // If this resource is type complete, it needs to be parsed together from tiles
-                    if (ceilingResource.equals(terrain.getCompleteResource())) {
-                        modelName += "_" + 0;
+                    Spatial ceiling;
+                    if (terrain.getFlags().contains(Terrain.TerrainFlag.CONSTRUCTION_TYPE_QUAD)) {
+
+                        // 2x2
+                        ceiling = new Node();
+//                        modelName += "_" + 1;
+                        for (int i = 0; i < 2; i++) {
+                            for (int k = 0; k < 2; k++) {
+                                Spatial ceilingPart = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + modelName + "_" + ((k % 2) + 1) + ".j3o");
+                                ceilingPart.move(i * 0.175f - 0.0875f, k * 0.175f - 0.0875f, 0);
+                                ((Node) ceiling).attachChild(ceilingPart);
+                            }
+                        }
+                        ceiling.setLocalScale(2, 2, 1);
+                    } else {
+                        ceiling = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + modelName + ".j3o");
+                    }
+                    ceiling.move(x * TILE_SIZE_X, y * TILE_SIZE_Y, (ceilingResource.equals(terrain.getCompleteResource()) ? -0.7f : 0));
+                    root.attachChild(ceiling);
+
+                    // See the wall status
+
+                    // North
+                    ArtResource wallNorth = getWallNorth(x, y, tiles, terrain);
+                    if (wallNorth != null) {
+                        Spatial wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallNorth.getName() + ".j3o");
+                        wall.move(0, 0.5f, -0.5f);
+                        addWall(wall, root, x, y);
                     }
 
-                    Spatial ceiling = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + modelName + ".j3o");
-                    ceiling.move(x * TILE_SIZE_X, y * TILE_SIZE_Y, 0);
-                    root.attachChild(ceiling);
-                }
+                    // South
+                    ArtResource wallSouth = getWallSouth(x, y, tiles, terrain);
+                    if (wallSouth != null) {
+                        Spatial wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallSouth.getName() + ".j3o");
+                        wall.move(0, -0.5f, -0.5f);
+                        Quaternion quat = new Quaternion();
+                        quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                        wall.rotate(quat);
+                        addWall(wall, root, x, y);
+                    }
 
-                // See the wall status
-                Spatial wall = null;
+                    // East
+                    ArtResource wallEast = getWallEast(x, y, tiles, terrain);
+                    if (wallEast != null) {
+                        Spatial wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallEast.getName() + ".j3o");
+                        wall.move(-0.5f, 0, -0.5f);
+                        Quaternion quat = new Quaternion();
+                        quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, 0, 1));
+                        wall.rotate(quat);
+                        addWall(wall, root, x, y);
+                    }
 
-                // North
-                ArtResource wallNorth = getWallNorth(x, y, tiles, terrain);
-                if (wallNorth != null) {
-                    wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallNorth.getName() + ".j3o");
-                    wall.move(0, 0.5f, -0.5f);
-                    addWall(wall, root, x, y);
-                }
+                    // West
+                    ArtResource wallWest = getWallWest(x, y, tiles, terrain);
+                    if (wallWest != null) {
+                        Spatial wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallWest.getName() + ".j3o");
+                        wall.move(0.5f, 0, -0.5f);
+                        Quaternion quat = new Quaternion();
+                        quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, 0, -1));
+                        wall.rotate(quat);
+                        addWall(wall, root, x, y);
+                    }
 
-                // South
-                ArtResource wallSouth = getWallSouth(x, y, tiles, terrain);
-                if (wallSouth != null) {
-                    wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallSouth.getName() + ".j3o");
-                    wall.move(0, -0.5f, -0.5f);
-                    Quaternion quat = new Quaternion();
-                    quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
-                    wall.rotate(quat);
-                    addWall(wall, root, x, y);
-                }
+                    //
+                } else if (terrain.getCompleteResource() != null) {
 
-                // East
-                ArtResource wallEast = getWallEast(x, y, tiles, terrain);
-                if (wallEast != null) {
-                    wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallEast.getName() + ".j3o");
-                    wall.move(-0.5f, 0, -0.5f);
-                    Quaternion quat = new Quaternion();
-                    quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, 0, 1));
-                    wall.rotate(quat);
-                    addWall(wall, root, x, y);
-                }
+                    // Floor, no ceiling, it has a floor
+                    ArtResource floorResource = terrain.getCompleteResource();
 
-                // West
-                ArtResource wallWest = getWallWest(x, y, tiles, terrain);
-                if (wallWest != null) {
-                    wall = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + wallWest.getName() + ".j3o");
-                    wall.move(0.5f, 0, -0.5f);
-                    Quaternion quat = new Quaternion();
-                    quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, 0, -1));
-                    wall.rotate(quat);
-                    addWall(wall, root, x, y);
-                }
+                    // For water construction type (lava & water), there are 8 pieces (0-7 suffix) in complete resource
+                    // And in the top resource there is the actual lava/water
+                    if (terrain.getFlags().contains(Terrain.TerrainFlag.CONSTRUCTION_TYPE_WATER)) {
 
-                //
-
-                if (terrain.getSideResource() != null) {
-                    // See wall need to next tile (since we have the
+                        // The bed
+                        Spatial floor = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + floorResource.getName() + "3" + ".j3o");
+                        floor.move(x * TILE_SIZE_X, y * TILE_SIZE_Y, -1f);
+                        root.attachChild(floor);
+                    } else if (terrain.getFlags().contains(Terrain.TerrainFlag.CONSTRUCTION_TYPE_QUAD)) {
+                    } else {
+                        Spatial floor = assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + floorResource.getName() + ".j3o");
+                        floor.move(x * TILE_SIZE_X, y * TILE_SIZE_Y, -1f);
+                        root.attachChild(floor);
+                    }
                 }
             }
         }
@@ -124,6 +151,10 @@ public class MapLoader implements ILoader<KwdFile> {
         Quaternion quat = new Quaternion();
         quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(1, 0, 0));
         root.rotate(quat);
+
+        // Batch it
+        root.batch();
+
         return root;
     }
 
@@ -153,7 +184,7 @@ public class MapLoader implements ILoader<KwdFile> {
         // The tile next to this needs to have it's own ceiling
         Terrain neigbourTerrain = kwdFile.getTerrain(tiles[x][y].getTerrainId());
         if (getCeilingResource(neigbourTerrain) == null) {
-            if (neigbourTerrain.getSideResource() != null) {
+            if (neigbourTerrain.getSideResource() != null && terrain.getFlags().contains(Terrain.TerrainFlag.ALLOW_ROOM_WALLS)) {
 
                 // Use neighbour wall
                 return neigbourTerrain.getSideResource();
@@ -176,11 +207,12 @@ public class MapLoader implements ILoader<KwdFile> {
     }
 
     private ArtResource getCeilingResource(Terrain terrain) {
-        if (terrain.getTopResource() != null && terrain.getTopResource().getSettings().getType() == ArtResource.Type.TERRAIN_MESH) {
-            return terrain.getTopResource(); // Normal
-        }
-        if (terrain.getFlags().contains(Terrain.TerrainFlag.SOLID) && terrain.getCompleteResource() != null && terrain.getCompleteResource().getSettings().getType() == ArtResource.Type.TERRAIN_MESH) {
-            return terrain.getCompleteResource(); // Reinforced wall
+        if (terrain.getFlags().contains(Terrain.TerrainFlag.SOLID)) {
+            if (terrain.getFlags().contains(Terrain.TerrainFlag.CONSTRUCTION_TYPE_QUAD)) {
+                return terrain.getCompleteResource(); // Claimed top
+            } else {
+                return terrain.getTopResource(); // Normal
+            }
         }
         return null;
     }
