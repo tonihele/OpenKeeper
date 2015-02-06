6545fd4c8d905672366333967eef6b36cc1c3a5a
Toni Helenius <helenius.toni@gmail.com>
2015-02-07 00:13:14 +0200

    Implemented JME asset loader for dictionaries

---
+++ src/toniarts/opendungeonkeeper/gui/plugins/DictLoader.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.gui.plugins;
+
+import com.jme3.asset.AssetInfo;
+import com.jme3.asset.AssetLoader;
+import java.io.IOException;
+import java.io.InputStream;
+import toniarts.opendungeonkeeper.gui.Dictionary;
+
+/**
+ * Asset loader for dictionaries (our own dictionary format)
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class DictLoader implements AssetLoader {
+
+    @Override
+    public Object load(AssetInfo assetInfo) throws IOException {
+        try (InputStream inputStream = assetInfo.openStream()) {
+            return new Dictionary(inputStream);
+        }
+    }
+}
