9e3af90934dff9b3338878d31903bfb8a9d7714e
Toni Helenius <helenius.toni@gmail.com>
2015-02-16 22:26:30 +0200

    Load up the front end type room, few issues remain but it is done enough for now, resolves #5

---
+++ src/toniarts/opendungeonkeeper/tools/convert/map/loader/room/HeroGateFrontEnd.java
@@ -0,0 +1,206 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toniarts.opendungeonkeeper.tools.convert.map.loader.room;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.LoopMode;
+import com.jme3.asset.AssetManager;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import java.awt.Point;
+import toniarts.opendungeonkeeper.tools.convert.AssetsConverter;
+import toniarts.opendungeonkeeper.tools.convert.Utils;
+import toniarts.opendungeonkeeper.tools.convert.map.loader.MapLoader;
+
+/**
+ * Loads up a hero gate, front end edition. Main menu. Most of the objects are
+ * listed in the objects, but I don't see how they help<br>
+ * TODO: Effect on the gem holder & lightning, controls for the level selection
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class HeroGateFrontEnd {
+
+    private HeroGateFrontEnd() {
+    }
+
+    public static Spatial construct(AssetManager assetManager, RoomInstance roomInstance) {
+        Node n = new Node();
+
+        // The front end hero gate
+
+        // Contruct the tiles
+        int i = 1;
+        Point start = roomInstance.getCoordinates().get(0);
+        for (Point p : roomInstance.getCoordinates()) {
+            Node tile = (Node) assetManager.loadModel(Utils.getCanonicalAssetKey(AssetsConverter.MODELS_FOLDER + "/" + roomInstance.getRoom().getCompleteResource().getName() + i + ".j3o"));
+
+            // Reset
+            resetAndMoveSpatial(tile, start, p);
+
+            // Set the shadows
+            // TODO: optimize, set to individual pieces and see zExtend whether it casts or not
+            tile.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+
+            n.attachChild(tile);
+
+            // Add some objects according to the tile number
+            if (i == 2) {
+                n.attachChild(loadObject("3DFE_GemHolder", assetManager, start, p, false));
+
+                // The light beams, I dunno, there are maybe several of these here
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                n.attachChild(loadObject("3dfe_beams", assetManager, start, p, true).rotate(quat).move(0, 0.1f, -0.4f));
+
+                // TODO: Add a point/spot light here
+
+                // Banners
+                n.attachChild(loadObject("banner1_swing", assetManager, start, p, true));
+                n.attachChild(loadObject("banner2_swing", assetManager, start, p, true));
+
+                // The "candles"
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true));
+                quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true).rotate(quat).move(0, 1f, 0));
+            } else if (i == 5) {
+
+                // Banners
+                n.attachChild(loadObject("banner3_swing", assetManager, start, p, true));
+                n.attachChild(loadObject("banner4_swing", assetManager, start, p, true));
+
+                // The "candles"
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true));
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true).rotate(quat).move(0, 1f, 0));
+            } else if (i == 8) {
+
+                // Banners
+                n.attachChild(loadObject("banner1_swing", assetManager, start, p, true));
+                n.attachChild(loadObject("banner2_swing", assetManager, start, p, true));
+
+                // The "candles"
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true));
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true).rotate(quat).move(0, 1f, 0));
+            } else if (i == 11) {
+
+                // Banners
+                n.attachChild(loadObject("banner1_swing", assetManager, start, p, true));
+                n.attachChild(loadObject("banner2_swing", assetManager, start, p, true));
+
+                // The "candles"
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true));
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 0, 1));
+                n.attachChild(loadObject("chain_swing", assetManager, start, p, true).rotate(quat).move(0, 1f, 0));
+
+                // Map
+                for (int x = 1; x < 21; x++) {
+                    n.attachChild(loadObject("3dmap_level" + x, assetManager, start, p, false));
+                    if (x == 15) {
+                        n.attachChild(loadObject("3dmap_level" + x + "a", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "a" + "_arrows", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmap_level" + x + "b", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "b" + "_arrows", assetManager, start, p, false));
+                    } else if (x == 6) {
+                        n.attachChild(loadObject("3dmap_level" + x + "a", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "a" + "_arrows", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmap_level" + x + "b", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "b" + "_arrows", assetManager, start, p, false));
+                    } else if (x == 11) {
+                        n.attachChild(loadObject("3dmap_level" + x + "a", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "a" + "_arrows", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmap_level" + x + "b", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "b" + "_arrows", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmap_level" + x + "c", assetManager, start, p, false));
+                        n.attachChild(loadObject("3dmaplevel" + x + "c" + "_arrows", assetManager, start, p, false));
+                    } else {
+                        n.attachChild(loadObject("3dmaplevel" + x + "_arrows", assetManager, start, p, false));
+                    }
+                }
+
+                // Secret levels
+                for (int x = 1; x < 6; x++) {
+                    n.attachChild(loadObject("Secret_Level" + x, assetManager, start, p, false));
+                }
+
+                // The map base
+                n.attachChild(loadObject("3dmap_level21", assetManager, start, p, false));
+            }
+
+            i++;
+        }
+
+        // Set the transform and scale to our scale and 0 the transform
+        n.move(start.x * MapLoader.TILE_WIDTH - MapLoader.TILE_WIDTH / 2, start.y * MapLoader.TILE_HEIGHT - MapLoader.TILE_HEIGHT / 2, 0);
+        n.scale(MapLoader.TILE_WIDTH); // Squares anyway...
+
+        return n;
+    }
+
+    /**
+     * Resets (scale & translation) and moves the spatial to the point. The
+     * point is relative to the start point
+     *
+     * @param tile the tile, spatial
+     * @param start start point
+     * @param p the tile point
+     */
+    private static void resetAndMoveSpatial(Node tile, Point start, Point p) {
+
+        // Reset, really, the size is 1 after this...
+        for (Spatial subSpat : tile.getChildren()) {
+            subSpat.setLocalScale(1);
+            subSpat.setLocalTranslation(0, 0, 0);
+        }
+        tile.move(p.x - start.x, p.y - start.y, 1f);
+    }
+
+    /**
+     * Load an object asset
+     *
+     * @param model the model string
+     * @param assetManager the asset manager instance
+     * @param start starting point
+     * @param p the current point
+     * @param randomizeAnimation randomize object animation (speed and start
+     * time)
+     * @return reseted and ready to go model (also animated if there is a such
+     * option)
+     */
+    private static Spatial loadObject(String model, AssetManager assetManager, Point start, Point p, boolean randomizeAnimation) {
+        Node object = (Node) assetManager.loadModel(Utils.getCanonicalAssetKey(AssetsConverter.MODELS_FOLDER + "/" + model + ".j3o"));
+
+        // Reset
+        resetAndMoveSpatial(object, start, p);
+        object.move(0, 0, -1f);
+
+        // Animate
+        AnimControl animControl = (AnimControl) object.getChild(0).getControl(AnimControl.class);
+        if (animControl != null) {
+            AnimChannel channel = animControl.createChannel();
+            channel.setAnim("anim");
+            channel.setLoopMode(LoopMode.Loop);
+            if (randomizeAnimation) {
+                channel.setSpeed(FastMath.nextRandomInt(1, 10) / 10f);
+                channel.setTime(FastMath.nextRandomFloat() * channel.getAnimMaxTime());
+            }
+
+            // Don't batch animated objects, seems not to work
+            object.setBatchHint(Spatial.BatchHint.Never);
+        }
+
+        return object;
+    }
+}
