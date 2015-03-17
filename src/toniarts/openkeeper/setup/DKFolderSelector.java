ffedc5e946ba5913f987f9c3ecdf6b3088460482
Toni Helenius <helenius.toni@gmail.com>
2015-03-17 20:10:27 +0200

    Fixed window size

--- src/toniarts/openkeeper/setup/DKFolderSelector.java
+++ src/toniarts/openkeeper/setup/DKFolderSelector.java
@@ -101,7 +101,7 @@ public abstract class DKFolderSelector extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(statusIcon)
                     .addComponent(selectFolderButton))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(exitButton)
                     .addComponent(continueButton))
