d5d14964f3d81102a4972335808aaba502c150ac
Toni Helenius <helenius.toni@gmail.com>
2015-03-26 21:03:46 +0200

    Implemented calculation of the dequantization table

--- src/toniarts/openkeeper/video/tgq/TgqFrame.java
+++ src/toniarts/openkeeper/video/tgq/TgqFrame.java
@@ -21,7 +21,8 @@ import java.nio.ByteOrder;
 import toniarts.openkeeper.tools.convert.Utils;
 
 /**
- * Holds a TGQ frame (one texture that is)
+ * Holds a TGQ frame (one texture that is)<br>
+ * References: FFMPEG, eatgi.c
  *
  * @author Toni Helenius <helenius.toni@gmail.com>
  */
@@ -30,6 +31,25 @@ public class TgqFrame implements Comparable<TgqFrame> {
     private final int width;
     private final int height;
     private final int frameIndex;
+    private int[] dequantizationTable = new int[64];
+    private final static int[] baseTable = {
+        4096, 2953, 3135, 3483, 4096, 5213, 7568, 14846,
+        2953, 2129, 2260, 2511, 2953, 3759, 5457, 10703,
+        3135, 2260, 2399, 2666, 3135, 3990, 5793, 11363,
+        3483, 2511, 2666, 2962, 3483, 4433, 6436, 12625,
+        4096, 2953, 3135, 3483, 4096, 5213, 7568, 14846,
+        5213, 3759, 3990, 4433, 5213, 6635, 9633, 18895,
+        7568, 5457, 5793, 6436, 7568, 9633, 13985, 27432,
+        14846, 10703, 11363, 12625, 14846, 18895, 27432, 53809};
+    private final static int[] baseTable2 = {
+        8, 16, 19, 22, 26, 27, 29, 34,
+        16, 16, 22, 24, 27, 29, 34, 37,
+        19, 22, 26, 27, 29, 34, 34, 38,
+        22, 22, 26, 27, 29, 34, 37, 40,
+        22, 26, 27, 29, 32, 35, 40, 48,
+        26, 27, 29, 32, 35, 40, 48, 58,
+        26, 27, 29, 34, 38, 46, 56, 69,
+        27, 29, 35, 38, 46, 56, 69, 83};
 
     public TgqFrame(byte[] data, int frameIndex) {
         this.frameIndex = frameIndex;
@@ -47,6 +67,17 @@ public class TgqFrame implements Comparable<TgqFrame> {
     private void decodeFrame(ByteBuffer buf) {
         short quantizer = Utils.toUnsignedByte(buf.get());
         buf.position(buf.position() + 3); // Skip 3 bytes
+        calculateDequantizationTable(quantizer);
+    }
+
+    private void calculateDequantizationTable(short quantizer) {
+
+        // This is different from the specs -> can use integer math
+        int qScale = (215 - 2 * quantizer) * 5;
+        dequantizationTable[0] = (baseTable[0] * baseTable2[0]) >> 11;
+        for (int i = 1; i < 64; i++) {
+            dequantizationTable[i] = (baseTable[i] * baseTable2[i] * qScale + 32) >> 14;
+        }
     }
 
     public int getWidth() {
