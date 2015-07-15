4fea795f6af04aeec4c98df11216e14c67c08afa
Toni Helenius <helenius.toni@gmail.com>
2015-07-15 21:52:00 +0300

    Merged DK II hiscore reading (small changes) by ArchDemon

---
+++ src/toniarts/openkeeper/tools/convert/hiscores/HiScoresEntry.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package toniarts.openkeeper.tools.convert.hiscores;
+
+/**
+ * Stores the HiScores.dat file entry structure<br>
+ *
+ * @author ArchDemon
+ */
+public class HiScoresEntry {
+
+    private int score;
+    private String name;
+    private String level;
+
+    public int getScore() {
+        return score;
+    }
+
+    protected void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    protected void setLevel(String level) {
+        this.level = level;
+    }
+}
---
+++ src/toniarts/openkeeper/tools/convert/hiscores/HiScoresFile.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package toniarts.openkeeper.tools.convert.hiscores;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+import toniarts.openkeeper.tools.convert.Utils;
+
+/**
+ * Stores the HiScores file entries<br>
+ * Actual format reverse engineered by ArchDemon
+ *
+ * @author ArchDemon
+ */
+public class HiScoresFile {
+
+    private final List<HiScoresEntry> hiScoresEntries;
+
+    /**
+     * Constructs a new HiScores file reader<br>
+     * Reads the HiScores.dat file
+     *
+     * @param file the HiScores file to read
+     */
+    public HiScoresFile(File file) {
+
+        //Read the file
+        try (RandomAccessFile data = new RandomAccessFile(file, "r")) {
+
+            //Read the entries, no header, just entries till the end
+            hiScoresEntries = new ArrayList<>();
+            while (data.getFilePointer() < data.length()) {
+                HiScoresEntry entry = new HiScoresEntry();
+                entry.setScore(Utils.readUnsignedInteger(data));
+                entry.setName(Utils.readVaryingLengthStringUtf16(data, 64).trim());
+                entry.setLevel(Utils.readVaryingLengthStringUtf16(data, 64).trim());
+
+                hiScoresEntries.add(entry);
+            }
+        } catch (IOException e) {
+
+            //Fug
+            throw new RuntimeException("Failed to open the file " + file + "!", e);
+        }
+    }
+
+    public List<HiScoresEntry> getHiScoresEntries() {
+        return hiScoresEntries;
+    }
+}
