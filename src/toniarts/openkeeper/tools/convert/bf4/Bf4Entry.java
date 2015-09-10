55876cfd04eadb1ac50a96707f2b122e47b78aa4
Toni Helenius <helenius.toni@gmail.com>
2015-09-10 23:12:00 +0300

    Initial DK II font file reading, with a test class

---
+++ src/toniarts/openkeeper/tools/convert/bf4/Bf4Entry.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package toniarts.openkeeper.tools.convert.bf4;
+
+/**
+ * BF4 entry a.k.a. FontEntry
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class Bf4Entry {
+
+    public enum FontEntryFlag {
+
+        RLE4_DATA, UNKNOWN;
+
+        /**
+         * Font entry flag value to enum
+         *
+         * @param index the flag value
+         * @return returns flag enum
+         */
+        public static FontEntryFlag toFontEntryFlag(int index) {
+            if (index == 0) {
+                return RLE4_DATA;
+            }
+            if (index == 1) {
+                return UNKNOWN;
+            }
+            throw new RuntimeException("Flag must be 0 or 1! Was " + index + "!");
+        }
+    }
+    private Character character;
+    private int unknown1;
+    private int dataSize;
+    private int totalSize;
+    private FontEntryFlag flag;
+    private short unknown2;
+    private short unknown3;
+    private short unknown4;
+    private int width;
+    private int height;
+    private byte offsetX;
+    private byte offsetY;
+    private int outerWidth;
+    private byte[] imageData;
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    protected void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public int getUnknown1() {
+        return unknown1;
+    }
+
+    protected void setUnknown1(int unknown1) {
+        this.unknown1 = unknown1;
+    }
+
+    public int getDataSize() {
+        return dataSize;
+    }
+
+    protected void setDataSize(int dataSize) {
+        this.dataSize = dataSize;
+    }
+
+    public int getTotalSize() {
+        return totalSize;
+    }
+
+    protected void setTotalSize(int totalSize) {
+        this.totalSize = totalSize;
+    }
+
+    public FontEntryFlag getFlag() {
+        return flag;
+    }
+
+    protected void setFlag(FontEntryFlag flag) {
+        this.flag = flag;
+    }
+
+    public short getUnknown2() {
+        return unknown2;
+    }
+
+    protected void setUnknown2(short unknown2) {
+        this.unknown2 = unknown2;
+    }
+
+    public short getUnknown3() {
+        return unknown3;
+    }
+
+    protected void setUnknown3(short unknown3) {
+        this.unknown3 = unknown3;
+    }
+
+    public short getUnknown4() {
+        return unknown4;
+    }
+
+    protected void setUnknown4(short unknown4) {
+        this.unknown4 = unknown4;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    public byte getOffsetX() {
+        return offsetX;
+    }
+
+    protected void setOffsetX(byte offsetX) {
+        this.offsetX = offsetX;
+    }
+
+    public byte getOffsetY() {
+        return offsetY;
+    }
+
+    protected void setOffsetY(byte offsetY) {
+        this.offsetY = offsetY;
+    }
+
+    public int getOuterWidth() {
+        return outerWidth;
+    }
+
+    protected void setOuterWidth(int outerWidth) {
+        this.outerWidth = outerWidth;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    protected void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    @Override
+    public String toString() {
+        return character.toString();
+    }
+}
---
+++ src/toniarts/openkeeper/tools/convert/bf4/Bf4File.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package toniarts.openkeeper.tools.convert.bf4;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+import toniarts.openkeeper.tools.convert.ConversionUtils;
+import toniarts.openkeeper.tools.convert.bf4.Bf4Entry.FontEntryFlag;
+
+/**
+ * Reads the Dungeon Keeper 2 BF4 files, bitmap fonts that is<br>
+ * Format reverse engineered by:
+ * <li>George Gensure</li>
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public class Bf4File {
+
+    private static final String BF4_HEADER_IDENTIFIER = "F4FB";
+    private final List<Bf4Entry> entries;
+
+    /**
+     * Constructs a new BF4 file reader Reads the BF4 file structure
+     *
+     * @param file the bf4 file to read
+     */
+    public Bf4File(File file) {
+
+        // Read the file
+        try (RandomAccessFile rawBf4 = new RandomAccessFile(file, "r")) {
+
+            // Check the header
+            byte[] header = new byte[4];
+            rawBf4.read(header);
+            if (!BF4_HEADER_IDENTIFIER.equals(ConversionUtils.bytesToString(header))) {
+                throw new RuntimeException("Header should be " + BF4_HEADER_IDENTIFIER + " and it was " + header + "! Cancelling!");
+            }
+            short maxWidth = ConversionUtils.toUnsignedByte(rawBf4.readByte()); // This is know to be bogus value
+            short maxHeight = ConversionUtils.toUnsignedByte(rawBf4.readByte());
+            int offsetsCount = ConversionUtils.readUnsignedShort(rawBf4);
+
+            // Read the offsets
+            List<Integer> offsets = new ArrayList<>(offsetsCount);
+            for (int i = 0; i < offsetsCount; i++) {
+                offsets.add(ConversionUtils.readUnsignedInteger(rawBf4));
+            }
+
+            // Read the font entries
+            entries = new ArrayList<>(offsetsCount);
+            for (Integer offset : offsets) {
+                rawBf4.seek(offset);
+                entries.add(readFontEntry(rawBf4));
+            }
+        } catch (IOException e) {
+
+            // Fug
+            throw new RuntimeException("Failed to read the file " + file + "!", e);
+        }
+    }
+
+    /**
+     * Reads a single font entry from the file
+     *
+     * @param rawBf4 the file
+     * @return the font entry
+     * @throws IOException may fail
+     */
+    private Bf4Entry readFontEntry(RandomAccessFile rawBf4) throws IOException {
+        Bf4Entry entry = new Bf4Entry();
+        byte[] bytes = new byte[2];
+        rawBf4.read(bytes);
+        entry.setCharacter(ConversionUtils.bytesToStringUtf16(bytes).charAt(0));
+        entry.setUnknown1(ConversionUtils.readUnsignedShort(rawBf4));
+        entry.setDataSize(ConversionUtils.readInteger(rawBf4));
+        entry.setTotalSize(ConversionUtils.readUnsignedInteger(rawBf4));
+        entry.setFlag(FontEntryFlag.toFontEntryFlag(ConversionUtils.toUnsignedByte(rawBf4.readByte())));
+        entry.setUnknown2(ConversionUtils.toUnsignedByte(rawBf4.readByte()));
+        entry.setUnknown3(ConversionUtils.toUnsignedByte(rawBf4.readByte()));
+        entry.setUnknown4(ConversionUtils.toUnsignedByte(rawBf4.readByte()));
+        entry.setWidth(ConversionUtils.readUnsignedShort(rawBf4));
+        entry.setHeight(ConversionUtils.readUnsignedShort(rawBf4));
+        entry.setOffsetX(rawBf4.readByte());
+        entry.setOffsetY(rawBf4.readByte());
+        entry.setOuterWidth(ConversionUtils.readShort(rawBf4));
+        bytes = new byte[entry.getDataSize()];
+        rawBf4.read(bytes);
+        return entry;
+    }
+}
