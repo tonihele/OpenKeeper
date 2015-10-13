d8939012d0e945514421960de84fa674f8863c3b
Toni Helenius <helenius.toni@gmail.com>
2015-10-13 21:57:16 +0300

    Added header

--- src/toniarts/openkeeper/world/PathFinder.java
+++ src/toniarts/openkeeper/world/PathFinder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package toniarts.openkeeper.world;
 
 import com.jme3.math.Vector2f;
@@ -7,41 +23,40 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import toniarts.openkeeper.tools.convert.map.KwdFile;
-import toniarts.openkeeper.tools.convert.map.Tile;
 import toniarts.openkeeper.tools.convert.map.Terrain;
+import toniarts.openkeeper.tools.convert.map.Tile;
 
 /**
  *
  * @author ArchDemon
  */
-
-
 public class PathFinder {
-    private Integer[][] tree; 
+
+    private Integer[][] tree;
     private final Integer[][] original;
     public Vector2f start;
     public Vector2f end;
     private Queue<Vector3f> queue;
-    
+
     public PathFinder(KwdFile kwdFile) {
-        
+
         Tile[][] tiles = kwdFile.getTiles();
         this.original = new Integer[tiles.length][tiles[0].length];
-        
+
         for (int x = 0; x < tiles.length; x++) {
             for (int y = 0; y < tiles[x].length; y++) {
-                
+
                 Terrain terrain = kwdFile.getTerrain(kwdFile.getTile(x, y).getTerrainId());
-                original[x][y] = (terrain.getFlags().contains(Terrain.TerrainFlag.SOLID)) ?  -1 : -2;                
+                original[x][y] = (terrain.getFlags().contains(Terrain.TerrainFlag.SOLID)) ? -1 : -2;
             }
         }
     }
-    
+
     public boolean hasPath(Vector2f from, Vector2f to) {
         if (from == null) {
             from = start;
         }
-        
+
         tree = new Integer[original.length][original[0].length];
         for (int x = 0; x < original.length; x++) {
             System.arraycopy(original[x], 0, tree[x], 0, original[x].length);
@@ -50,77 +65,77 @@ public class PathFinder {
         queue.add(new Vector3f(from.x, from.y, 0));
         while (!queue.isEmpty()) {
             Vector3f p = queue.poll();
-            this.fill((int)p.x, (int)p.y, (int)p.z, (int)to.x, (int)to.y);            
+            this.fill((int) p.x, (int) p.y, (int) p.z, (int) to.x, (int) to.y);
         }
 
-        return (tree[(int)to.x][(int)to.y] >= 0);
+        return (tree[(int) to.x][(int) to.y] >= 0);
     }
-    
+
     public void fill(int x, int y, int weight, int cx, int cy) {
-     
+
         tree[x][y] = weight;
         if (x == cx && y == cy) {
             queue.clear();
             return;
         }
 
-        Vector2f[] dir = { new Vector2f(1, 0), new Vector2f(-1, 0), new Vector2f(0, 1), new Vector2f(0, -1)};
+        Vector2f[] dir = {new Vector2f(1, 0), new Vector2f(-1, 0), new Vector2f(0, 1), new Vector2f(0, -1)};
 
         for (Vector2f d : dir) {
-            if ((x + d.x) < 0 || 
-                    (x + d.x) >= tree.length || 
-                    (y + d.y) < 0 || 
-                    (y + d.y) >= tree[(int)(x + d.x)].length) {
+            if ((x + d.x) < 0
+                    || (x + d.x) >= tree.length
+                    || (y + d.y) < 0
+                    || (y + d.y) >= tree[(int) (x + d.x)].length) {
                 continue;
             }
-            if (tree[(int)(x + d.x)][(int)(y + d.y)] == -2 || 
-                    tree[(int)(x + d.x)][(int)(y + d.y)] != -1 && 
-                    tree[(int)(x + d.x)][(int)(y + d.y)] > weight && 
-                    !queue.contains(new Vector3f(x + d.x, y + d.y, weight + 1))) {
-                tree[(int)(x + d.x)][(int)(y + d.y)] = weight + 1;
+            if (tree[(int) (x + d.x)][(int) (y + d.y)] == -2
+                    || tree[(int) (x + d.x)][(int) (y + d.y)] != -1
+                    && tree[(int) (x + d.x)][(int) (y + d.y)] > weight
+                    && !queue.contains(new Vector3f(x + d.x, y + d.y, weight + 1))) {
+                tree[(int) (x + d.x)][(int) (y + d.y)] = weight + 1;
                 queue.add(new Vector3f(x + d.x, y + d.y, weight + 1));
             }
-        } 
-        
+        }
+
     }
-    
+
     public List<Vector2f> getPath(Vector2f from, Vector2f to) {
         List<Vector2f> result = new ArrayList<>();
         if (!hasPath(from, to)) {
             return result;
         }
-        
+
         if (from == null) {
             from = start;
         }
-        
-        Vector2f[] dir = { new Vector2f(1, 0), new Vector2f(-1, 0), new Vector2f(0, 1), new Vector2f(0, -1)};
 
-        while(!from.equals(to)) {
-            int current = tree[(int)to.x][(int)to.y];
+        Vector2f[] dir = {new Vector2f(1, 0), new Vector2f(-1, 0), new Vector2f(0, 1), new Vector2f(0, -1)};
+
+        while (!from.equals(to)) {
+            int current = tree[(int) to.x][(int) to.y];
             int next;
             for (Vector2f d : dir) {
-                if ((to.x + d.x) < 0 || (to.x + d.x) >= tree.length || 
-                        (to.y + d.y) < 0 || (to.y + d.y) >= tree[(int)(to.x + d.x)].length) {
+                if ((to.x + d.x) < 0 || (to.x + d.x) >= tree.length
+                        || (to.y + d.y) < 0 || (to.y + d.y) >= tree[(int) (to.x + d.x)].length) {
                     continue;
-                }                
-                
-                next = tree[(int)(to.x + d.x)][(int)(to.y + d.y)];
-                
-                if (next >=0 && next < current) {                    
+                }
+
+                next = tree[(int) (to.x + d.x)][(int) (to.y + d.y)];
+
+                if (next >= 0 && next < current) {
                     to.addLocal(d);
                     result.add(to.clone());
                     break;
                 }
-            }    
-            
+            }
+
         }
-        
+
         return result;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         String result = "\n";
         for (int x = 0; x < tree.length; x++) {
             for (int y = 0; y < tree[x].length; y++) {
@@ -130,5 +145,4 @@ public class PathFinder {
         }
         return result;
     }
-    
 }
--- src/toniarts/openkeeper/world/ThingLoader.java
+++ src/toniarts/openkeeper/world/ThingLoader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package toniarts.openkeeper.world;
 
 import com.jme3.asset.AssetManager;
@@ -6,14 +22,13 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import toniarts.openkeeper.tools.convert.AssetsConverter;
 import toniarts.openkeeper.tools.convert.map.KwdFile;
-import toniarts.openkeeper.tools.convert.map.Thing;
 import toniarts.openkeeper.tools.convert.map.Object;
+import toniarts.openkeeper.tools.convert.map.Thing;
+
 /**
  *
  * @author ArchDemon
  */
-
-
 public class ThingLoader {
 
     public Spatial load(BulletAppState bulletAppState, AssetManager assetManager, KwdFile kwdFile) {
@@ -22,27 +37,26 @@ public class ThingLoader {
         Node root = new Node("Things");
         Node nodeCreatures = new Node("Creatures");
         Node nodeObjects = new Node("Objects");
-        for (toniarts.openkeeper.tools.convert.map.Thing obj : kwdFile.getThings())
-        {
-            try {            
+        for (toniarts.openkeeper.tools.convert.map.Thing obj : kwdFile.getThings()) {
+            try {
                 if (obj instanceof Thing.Creature) {
 
-                    Thing.Creature cr = (Thing.Creature)obj;
-                    GameCreature creature = new GameCreature(bulletAppState, assetManager, (Thing.Creature)obj, kwdFile);
-                
+                    Thing.Creature cr = (Thing.Creature) obj;
+                    GameCreature creature = new GameCreature(bulletAppState, assetManager, (Thing.Creature) obj, kwdFile);
+
                     nodeCreatures.attachChild(creature);
-                    
+
                 } else if (obj instanceof Thing.Object) {
 
-                        Thing.Object objectThing = (Thing.Object)obj;
-                        Object object = kwdFile.getObject(objectThing.getObjectId());
+                    Thing.Object objectThing = (Thing.Object) obj;
+                    Object object = kwdFile.getObject(objectThing.getObjectId());
 
-                        Node nodeObject = (Node) assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + object.getMeshResource().getName() + ".j3o"); 
-                        nodeObject.setLocalTranslation(
-                                objectThing.getPosX() * MapLoader.TILE_WIDTH, 
-                                0 * MapLoader.TILE_HEIGHT, 
-                                objectThing.getPosY() * MapLoader.TILE_WIDTH);
-                        nodeObjects.attachChild(nodeObject);
+                    Node nodeObject = (Node) assetManager.loadModel(AssetsConverter.MODELS_FOLDER + "/" + object.getMeshResource().getName() + ".j3o");
+                    nodeObject.setLocalTranslation(
+                            objectThing.getPosX() * MapLoader.TILE_WIDTH,
+                            0 * MapLoader.TILE_HEIGHT,
+                            objectThing.getPosY() * MapLoader.TILE_WIDTH);
+                    nodeObjects.attachChild(nodeObject);
 
                 }
             } catch (Exception ex) {
@@ -53,6 +67,6 @@ public class ThingLoader {
         root.attachChild(nodeCreatures);
         root.attachChild(nodeObjects);
         return root;
-       
+
     }
 }
