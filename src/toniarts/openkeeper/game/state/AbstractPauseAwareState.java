e8cf531bf4f065f1f7fe9c15c1abf00e8ad8a545
Toni Helenius <helenius.toni@gmail.com>
2015-10-28 22:23:59 +0200

    Added a state for identifying the states that should be disabled when pausing the game

---
+++ src/toniarts/openkeeper/game/state/AbstractPauseAwareState.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014-2015 OpenKeeper
+ *
+ * OpenKeeper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenKeeper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenKeeper.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package toniarts.openkeeper.game.state;
+
+import com.jme3.app.state.AbstractAppState;
+
+/**
+ * Just a simple wrapper for the game states to tell us should they be paused
+ * (disabled) when the game is paused
+ *
+ * @author Toni Helenius <helenius.toni@gmail.com>
+ */
+public abstract class AbstractPauseAwareState extends AbstractAppState {
+
+    /**
+     * Should this state be disabled when game is paused?
+     *
+     * @return true for disabling this state on pause
+     */
+    public abstract boolean isPauseable();
+}
