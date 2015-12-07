f73434a36932086ecb23b4330acf84920de5c076
Toni Helenius <helenius.toni@gmail.com>
2015-12-07 22:33:58 +0200

    Pillars for workshop (yes, they clip to the walls in the original too)

--- src/toniarts/openkeeper/world/room/Workshop.java
+++ src/toniarts/openkeeper/world/room/Workshop.java
@@ -17,8 +17,16 @@
 package toniarts.openkeeper.world.room;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.sun.istack.internal.Nullable;
+import java.awt.Point;
+import java.util.EnumSet;
 import toniarts.openkeeper.tools.convert.map.Thing;
+import toniarts.openkeeper.world.MapLoader;
 
 /**
  * The workshop
@@ -33,6 +41,61 @@ public class Workshop extends Normal {
 
     @Override
     protected void contructPillars(Node node) {
-        // We have very different logic
+
+        // We have very different logic than the normal
+        // Go through all the points and see if they are fit for pillar placement
+        for (Point p : roomInstance.getCoordinates()) {
+
+            // See that we have 2 "free" neigbouring tiles
+            EnumSet<WallSection.WallDirection> freeDirections = EnumSet.noneOf(WallSection.WallDirection.class);
+            if (!hasSameTile(map, p.x - start.x, p.y - start.y - 1)) { // North
+                freeDirections.add(WallSection.WallDirection.NORTH);
+            }
+            if (!hasSameTile(map, p.x - start.x, p.y - start.y + 1)) { // South
+                freeDirections.add(WallSection.WallDirection.SOUTH);
+            }
+            if (!hasSameTile(map, p.x - start.x + 1, p.y - start.y)) { // East
+                freeDirections.add(WallSection.WallDirection.EAST);
+            }
+            if (!hasSameTile(map, p.x - start.x - 1, p.y - start.y)) { // West
+                freeDirections.add(WallSection.WallDirection.WEST);
+            }
+
+            // We may have up to 4 pillars in the same tile even, every corner gets one, no need to check anything else
+
+            // Add a pillar
+
+            // Face "in" diagonally
+            if (freeDirections.contains(WallSection.WallDirection.NORTH) && freeDirections.contains(WallSection.WallDirection.EAST)) {
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, -1, 0));
+                constructPillar(node, p, quat).move(-0.15f, MapLoader.TILE_HEIGHT, -0.85f);
+            }
+            if (freeDirections.contains(WallSection.WallDirection.SOUTH) && freeDirections.contains(WallSection.WallDirection.EAST)) {
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI, new Vector3f(0, 1, 0));
+                constructPillar(node, p, quat).move(-0.15f, MapLoader.TILE_HEIGHT, -0.15f);
+            }
+            if (freeDirections.contains(WallSection.WallDirection.SOUTH) && freeDirections.contains(WallSection.WallDirection.WEST)) {
+                Quaternion quat = new Quaternion();
+                quat.fromAngleAxis(FastMath.PI / 2, new Vector3f(0, 1, 0));
+                constructPillar(node, p, quat).move(-0.85f, MapLoader.TILE_HEIGHT, -0.15f);
+            }
+            if (freeDirections.contains(WallSection.WallDirection.NORTH) && freeDirections.contains(WallSection.WallDirection.WEST)) {
+                constructPillar(node, p, null).move(-0.85f, MapLoader.TILE_HEIGHT, -0.85f);
+            }
+        }
+    }
+
+    private Spatial constructPillar(Node node, Point p, @Nullable Quaternion quat) {
+        Spatial part = assetManager.loadModel(getPillarResource());
+        resetAndMoveSpatial(part, new Point(0, 0), p);
+
+        if (quat != null) {
+            part.rotate(quat);
+        }
+
+        node.attachChild(part);
+        return part;
     }
 }
