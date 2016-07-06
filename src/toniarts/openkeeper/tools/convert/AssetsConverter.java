30f03624e4b2e347355698ab6c298589b8024932
Toni Helenius <helenius.toni@gmail.com>
2016-07-06 16:43:41 +0300

    Bump model version

--- src/toniarts/openkeeper/tools/convert/AssetsConverter.java
+++ src/toniarts/openkeeper/tools/convert/AssetsConverter.java
@@ -91,7 +91,7 @@ public abstract class AssetsConverter {
     public enum ConvertProcess {
 
         TEXTURES(4),
-        MODELS(3),
+        MODELS(4),
         MOUSE_CURSORS(3),
         MUSIC_AND_SOUNDS(2),
         INTERFACE_TEXTS(2),
