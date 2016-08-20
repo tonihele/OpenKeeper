c06a13c1995f23f79466af814b6f74423c5da55f
Toni Helenius <helenius.toni+github@gmail.com>
2016-08-20 20:21:29 +0300

    Picked up state

--- src/toniarts/openkeeper/ai/creature/CreatureState.java
+++ src/toniarts/openkeeper/ai/creature/CreatureState.java
@@ -260,6 +260,29 @@ public enum CreatureState implements State<CreatureControl> {
                     return true;
                 }
 
+            },
+    PICKED_UP {
+
+                @Override
+                public void enter(CreatureControl entity) {
+
+                }
+
+                @Override
+                public void update(CreatureControl entity) {
+
+                }
+
+                @Override
+                public void exit(CreatureControl entity) {
+
+                }
+
+                @Override
+                public boolean onMessage(CreatureControl entity, Telegram telegram) {
+                    return true;
+                }
+
             }
 
 }
