6cb753a64ca3862eab7ca681df7c339a44669e89
Toni Helenius <helenius.toni+github@gmail.com>
2016-08-31 21:55:41 +0300

    All all effects & objects to room, their ordering is critical

--- src/toniarts/openkeeper/tools/convert/map/KwdFile.java
+++ src/toniarts/openkeeper/tools/convert/map/KwdFile.java
@@ -945,9 +945,7 @@ public final class KwdFile {
             List<Integer> roomEffects = new ArrayList<>(8);
             for (int x = 0; x < 8; x++) {
                 int effectId = ConversionUtils.readUnsignedShort(file);
-                if (effectId > 0) {
-                    roomEffects.add(effectId);
-                }
+                roomEffects.add(effectId);
             }
             room.setEffects(roomEffects);
             room.setRoomId((short) file.readUnsignedByte());
@@ -959,9 +957,7 @@ public final class KwdFile {
             List<Short> roomObjects = new ArrayList<>(8);
             for (int x = 0; x < 8; x++) {
                 short objectId = (short) file.readUnsignedByte();
-                if (objectId > 0) {
-                    roomObjects.add(objectId);
-                }
+                roomObjects.add(objectId);
             }
             room.setObjects(roomObjects);
 
