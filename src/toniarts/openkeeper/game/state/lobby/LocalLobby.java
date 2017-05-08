3a5e29f02979942cbef85c74b090387ccd943372
Toni Helenius <helenius.toni+github@gmail.com>
2017-05-08 21:23:45 +0300

    A bit refactoring

--- src/toniarts/openkeeper/game/state/lobby/LocalLobby.java
+++ src/toniarts/openkeeper/game/state/lobby/LocalLobby.java
@@ -22,8 +22,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import static java.util.stream.Collectors.toList;
 import toniarts.openkeeper.game.data.Keeper;
 import toniarts.openkeeper.tools.convert.map.AI;
 import toniarts.openkeeper.utils.Utils;
@@ -64,7 +62,7 @@ public class LocalLobby implements LobbyService, LobbyClientService {
     public void setMap(String mapName, int maxPlayers) {
         this.map = mapName;
         this.maxPlayers = maxPlayers;
-        for (ClientInfo clientInfo : compactPlayers(new HashSet<>(players.values()), maxPlayers)) {
+        for (ClientInfo clientInfo : LobbyUtils.compactPlayers(new HashSet<>(players.values()), maxPlayers)) {
             players.remove(clientInfo.getId());
         }
         for (LobbySessionListener l : listeners) {
@@ -76,7 +74,7 @@ public class LocalLobby implements LobbyService, LobbyClientService {
     @Override
     public void addPlayer() {
         if (players.size() < maxPlayers) {
-            ClientInfo clientInfo = createClientInfo(getNextKeeper(true, new HashSet<>(players.values())), Utils.getMainTextResourceBundle().getString("58"));
+            ClientInfo clientInfo = createClientInfo(LobbyUtils.getNextKeeper(true, new HashSet<>(players.values())), Utils.getMainTextResourceBundle().getString("58"));
             players.put(clientInfo.getId(), clientInfo);
             notifyPlayerListChange();
         }
@@ -138,65 +136,4 @@ public class LocalLobby implements LobbyService, LobbyClientService {
     public int getPlayerId() {
         return 0;
     }
-
-    /**
-     * Get the next available keeper
-     *
-     * @param ai AI or human
-     * @param players the current player list
-     * @return the next available Keeper
-     */
-    public static Keeper getNextKeeper(boolean ai, Set<ClientInfo> players) {
-        short id = Keeper.KEEPER1_ID;
-        List<Short> keepers = players.stream().map(c -> c.getKeeper().getId()).collect(toList());
-        Collections.sort(keepers);
-        while (Collections.binarySearch(keepers, id) >= 0) {
-            id++;
-        }
-        return new Keeper(ai, id, null);
-    }
-
-    /**
-     * Kicks out AI players that do not fit to the map, fixes Keepers so that
-     * they are in order
-     *
-     * @param players the player list
-     * @param maxPlayers the max player count
-     * @return a list of players to kick
-     */
-    public static Set<ClientInfo> compactPlayers(Set<ClientInfo> players, int maxPlayers) {
-        Set<ClientInfo> kickedPlayers = Collections.emptySet();
-
-        // Kick excessive players (AI only)
-        if (players.size() > maxPlayers) {
-            List<ClientInfo> keepers = new ArrayList<>(players);
-            Collections.sort(keepers, (ClientInfo o1, ClientInfo o2) -> Short.compare(o2.getKeeper().getId(), o1.getKeeper().getId()));
-            int playersToKick = players.size() - maxPlayers;
-            int playersKicked = 0;
-            kickedPlayers = new HashSet<>(playersToKick);
-            for (ClientInfo keeper : keepers) {
-                if (keeper.getKeeper().isAi()) {
-                    kickedPlayers.add(keeper);
-                    playersKicked++;
-                }
-                if (playersKicked == playersToKick) {
-                    break;
-                }
-            }
-        }
-
-        // Compact the keeper IDs
-        List<ClientInfo> keepers = new ArrayList<>(players);
-        Collections.sort(keepers, (ClientInfo o1, ClientInfo o2) -> Short.compare(o1.getKeeper().getId(), o2.getKeeper().getId()));
-        short id = Keeper.KEEPER1_ID;
-        for (ClientInfo keeper : keepers) {
-            if (!kickedPlayers.contains(keeper)) {
-                keeper.getKeeper().setId(id);
-                id++;
-            }
-        }
-
-        return kickedPlayers;
-    }
-
 }
