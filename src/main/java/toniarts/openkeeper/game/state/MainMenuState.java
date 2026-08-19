a75fda7bff2da8490b1bdfc8be9087d94823684b
ufdada <ufdada@gmail.com>
2026-08-19 21:04:26 +0200

    change debriefing to private, fixes deep source warning

--- src/main/java/toniarts/openkeeper/game/state/MainMenuState.java
+++ src/main/java/toniarts/openkeeper/game/state/MainMenuState.java
@@ -93,7 +93,7 @@ public final class MainMenuState extends AbstractAppState {
     protected Node menuNode;
     protected GeneralLevel selectedLevel;
     protected AudioNode levelBriefing;
-    protected AudioNode levelDebriefing;
+    private AudioNode levelDebriefing;
 
     private KwdFile kwdFile;
     protected final MainMenuInteraction listener;
