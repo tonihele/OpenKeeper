5b23bd10ec22e70e58ad345f9c39ea88cff5dbfc
wietse <wietse.jorissen@gmail.com>
2026-09-06 14:17:33 +0200

    Show creature mood in health flowers

--- src/main/java/toniarts/openkeeper/view/control/UnitFlowerControl.java
+++ src/main/java/toniarts/openkeeper/view/control/UnitFlowerControl.java
@@ -228,6 +228,17 @@ public abstract class UnitFlowerControl<T> extends BillboardControl implements I
     }
 
     /**
+     * Whether this flower must remain visible without an explicit call to
+     * {@link #show()}. Subclasses can use this for persistent native status
+     * indications.
+     *
+     * @return true when the flower should be visible
+     */
+    protected boolean isPersistentVisibilityRequired() {
+        return false;
+    }
+
+    /**
      * Show the flower for a brief time
      */
     @Override
@@ -283,6 +294,10 @@ public abstract class UnitFlowerControl<T> extends BillboardControl implements I
     public void update(float tpf) {
         entity.applyChanges();
 
+        if (!isEnabled() && unitSpatial != null && isPersistentVisibilityRequired()) {
+            show();
+        }
+
         super.update(tpf);
     }
 
@@ -291,7 +306,7 @@ public abstract class UnitFlowerControl<T> extends BillboardControl implements I
         super.controlUpdate(tpf);
 
         timeVisible += tpf;
-        if (timeVisible >= targetTimeVisible) {
+        if (timeVisible >= targetTimeVisible && !isPersistentVisibilityRequired()) {
 
             // Remove us
             hide();
